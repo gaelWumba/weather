4cf96bb3029e9527e155ad2356985c62a12221b9
gaelWumba <gael.wumbabuatu@gmail.com>
2024-06-10 10:15:51 +0200

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by gael on 10/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

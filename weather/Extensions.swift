50154b7cf39e3489e9df3d4b99549797a46485b4
gaelWumba <gael.wumbabuatu@gmail.com>
2024-07-18 13:39:09 +0200

    Data fetch

---
+++ weather/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  weather
+//
+//  Created by gael on 18/07/2024.
+//
+
+import Foundation
---
+++ weather/Preview Content/ModelData.swift
@@ -0,0 +1,8 @@
+//
+//  ModelData.swift
+//  weather
+//
+//  Created by gael on 18/07/2024.
+//
+
+import Foundation
